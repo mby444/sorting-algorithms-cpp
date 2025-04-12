b8d745dda27388a140a0f5d69799c97175f1765d
mby444 <bimayudha161@gmail.com>
2025-04-12 15:34:06 +0700

    Added insertion sort

--- insertion.cpp
+++ insertion.cpp
@@ -12,6 +12,22 @@ void printArray(int arr[], int n)
 
 void insertionSort(int arr[], int n)
 {
+    int key;
+    int j;
+
+    for (int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (arr[j] > key && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
 }
 
 int main()
